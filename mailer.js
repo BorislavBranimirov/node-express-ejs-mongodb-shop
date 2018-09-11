612eefbbe12cd0820442ddef8648dde2f8d3f473
Borislav Branimirov <boriintorr@gmail.com>
2018-09-11 11:28:43 +0300

    Fixed mailer.js and formatting

--- mailer.js
+++ mailer.js
@@ -1,9 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    service: 'Gmail',
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASS
